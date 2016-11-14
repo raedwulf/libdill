66cbdb84751463424c73899b7bad0ef82dbda087
Tai Chi Minh Ralph Eastwood <tcmreastwood@gmail.com>
2016-11-14 10:18:07 +0000

    Experimental simplified stack switching assembly

--- libdill.h
+++ libdill.h
@@ -117,38 +117,23 @@ DILL_EXPORT void dill_proc_epilogue(void);
     int ret;\
     asm("lea     LJMPRET%=(%%rip), %%rcx\n\t"\
         "xor     %%rax, %%rax\n\t"\
-        "mov     %%rbx, (%%rdx)\n\t"\
-        "mov     %%rbp, 8(%%rdx)\n\t"\
-        "mov     %%r12, 16(%%rdx)\n\t"\
-        "mov     %%r13, 24(%%rdx)\n\t"\
-        "mov     %%r14, 32(%%rdx)\n\t"\
-        "mov     %%r15, 40(%%rdx)\n\t"\
-        "mov     %%rsp, 48(%%rdx)\n\t"\
-        "mov     %%rcx, 56(%%rdx)\n\t"\
+        "mov     %%rbp, (%%rdx)\n\t"\
+        "mov     %%rsp, 8(%%rdx)\n\t"\
+        "mov     %%rcx, 16(%%rdx)\n\t"\
         "LJMPRET%=:\n\t"\
         : "=a" (ret)\
         : "d" (ctx)\
-        : "memory", "rcx", "rsi", "rdi", "r8", "r9", "r10", "r11", "cc");\
+        : "memory", "rbx", "rcx", "rsi", "rdi", "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15", "cc");\
     ret;\
 })
 #define dill_longjmp(ctx) \
-    asm("movq   56(%%rdx), %%rcx\n\t"\
-        "movq   48(%%rdx), %%rsp\n\t"\
-        "movq   40(%%rdx), %%r15\n\t"\
-        "movq   32(%%rdx), %%r14\n\t"\
-        "movq   24(%%rdx), %%r13\n\t"\
-        "movq   16(%%rdx), %%r12\n\t"\
-        "movq   8(%%rdx), %%rbp\n\t"\
-        "movq   (%%rdx), %%rbx\n\t"\
+    asm("movq   16(%%rdx), %%rcx\n\t"\
+        "movq   8(%%rdx), %%rsp\n\t"\
+        "movq   (%%rdx), %%rbp\n\t"\
         ".cfi_def_cfa %%rdx, 0 \n\t"\
-        ".cfi_offset %%rbx, 0 \n\t"\
-        ".cfi_offset %%rbp, 8 \n\t"\
-        ".cfi_offset %%r12, 16 \n\t"\
-        ".cfi_offset %%r13, 24 \n\t"\
-        ".cfi_offset %%r14, 32 \n\t"\
-        ".cfi_offset %%r15, 40 \n\t"\
-        ".cfi_offset %%rsp, 48 \n\t"\
-        ".cfi_offset %%rip, 56 \n\t"\
+        ".cfi_offset %%rbp, 0 \n\t"\
+        ".cfi_offset %%rsp, 8 \n\t"\
+        ".cfi_offset %%rip, 16 \n\t"\
         "jmp    *%%rcx\n\t"\
         : : "d" (ctx), "a" (1))
 #define DILL_SETSP(x) \
